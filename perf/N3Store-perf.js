97c2e5e01aae11ccf6a5823f8804510be0dcbb5c
Victor Felder <victor@draft.li>
2019-12-23 17:16:39 +0100

    perf: fix perf script

--- perf/N3Store-perf.js
+++ perf/N3Store-perf.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --expose-gc
 const assert = require('assert')
 if (!assert.hasOwnProperty('strict')) {
   assert.strict = {
@@ -6,131 +6,143 @@ if (!assert.hasOwnProperty('strict')) {
     deepEqual: assert.deepEqual // eslint-disable-line
   }
 }
+
+function gc () {
+  try {
+    if (global.gc) { global.gc() }
+  } catch (e) {
+    console.log('`node --expose-gc index.js`')
+    process.exit()
+  }
+}
+
 const rdf = require('@rdfjs/data-model')
 
-const datasetFactory = require('..')
-let dataset = datasetFactory()
-
-// const rdfjsDataset = require('@rdfjs/dataset')
-// const datasetFactory = () => rdfjsDataset.dataset()
-// let dataset = datasetFactory()
-
-console.log('N3Store performance test')
+const implementations = {
+  '@vhf/rdf-dataset-indexed': require('..'),
+  '@rdfjs/dataset': require('@rdfjs/dataset')
+}
 
 const prefix = 'http://example.org/#'
 const prefixed = (term) => rdf.namedNode(`${prefix}${term}`)
 
 /* Test triples */
-let dim = parseInt(process.argv[2], 10) || 64
-let dimSquared = dim * dim
-let dimCubed = dimSquared * dim
+const dim = parseInt(process.argv[2], 10) || 64
+const dimSquared = dim ** 2
+const dimCubed = dim ** 3
 
-let TEST = `- Adding ${dimCubed} triples to the default graph`
-console.time(TEST)
-let i, j, k, l
-for (i = 0; i < dim; i++) {
-  for (j = 0; j < dim; j++) {
-    for (k = 0; k < dim; k++) {
-      dataset.add(
-        rdf.quad(
-          prefixed(i),
-          prefixed(j),
-          prefixed(k)
-        )
-      )
-    }
-  }
-}
-console.timeEnd(TEST)
-
-console.log(`* Memory usage for triples: ${Math.round(process.memoryUsage().rss / 1024 / 1024)}MB`)
-
-TEST = `- Finding all ${dimCubed} triples in the default graph ${dimSquared * 1} times (0 variables)`
-console.time(TEST)
-for (i = 0; i < dim; i++) {
-  for (j = 0; j < dim; j++) {
-    for (k = 0; k < dim; k++) {
-      assert.strict.equal(dataset.match(prefixed(i), prefixed(j), prefixed(k), '').size, 1)
-    }
-  }
-}
-console.timeEnd(TEST)
-
-TEST = `- Finding all ${dimCubed} triples in the default graph ${dimSquared * 2} times (1 variable)`
-console.time(TEST)
-for (i = 0; i < dim; i++) {
-  for (j = 0; j < dim; j++) {
-    assert.strict.equal(dataset.match(prefixed(i), prefixed(j), null, '').size, dim)
-  }
-}
-for (i = 0; i < dim; i++) {
-  for (j = 0; j < dim; j++) {
-    assert.strict.equal(dataset.match(prefixed(i), null, prefixed(j), '').size, dim)
-  }
-}
-for (i = 0; i < dim; i++) {
-  for (j = 0; j < dim; j++) {
-    assert.strict.equal(dataset.match(null, prefixed(i), prefixed(j), '').size, dim)
-  }
-}
-console.timeEnd(TEST)
-
-TEST = `- Finding all ${dimCubed} triples in the default graph ${dimSquared * 3} times (2 variables)`
-console.time(TEST)
-for (i = 0; i < dim; i++) {
-  assert.strict.equal(dataset.match(prefixed(i), null, null, '').size, dimSquared)
-}
-for (j = 0; j < dim; j++) {
-  assert.strict.equal(dataset.match(null, prefixed(j), null, '').size, dimSquared)
-}
-for (k = 0; k < dim; k++) {
-  assert.strict.equal(dataset.match(null, null, prefixed(k), '').size, dimSquared)
-}
-console.timeEnd(TEST)
-
-console.log()
-
-/* Test quads */
-dim /= 4
-dimSquared = dim * dim
-dimCubed = dimSquared * dim
-const dimQuads = dimCubed * dim
-
-dataset = datasetFactory()
-TEST = `- Adding ${dimQuads} quads`
-console.time(TEST)
-for (i = 0; i < dim; i++) {
-  for (j = 0; j < dim; j++) {
-    for (k = 0; k < dim; k++) {
-      for (l = 0; l < dim; l++) {
-        dataset.add(
-          rdf.quad(
-            prefixed(i),
-            prefixed(j),
-            prefixed(k),
-            prefixed(l)
+for (const [implementation, factory] of Object.entries(implementations)) {
+  for (let runs = 0; runs < 2; runs++) {
+    gc()
+    let dataset = factory.dataset()
+    console.log(`\n\n${implementation} performance test, run ${runs + 1}`)
+    let TEST = `- Adding ${dimCubed} triples to the default graph`
+    console.time(TEST)
+    let i, j, k, l
+    for (i = 0; i < dim; i++) {
+      for (j = 0; j < dim; j++) {
+        for (k = 0; k < dim; k++) {
+          dataset.add(
+            rdf.quad(
+              prefixed(i),
+              prefixed(j),
+              prefixed(k)
+            )
           )
-        )
+        }
       }
     }
+    console.timeEnd(TEST)
+
+    console.log(`* Memory usage for triples: ${Math.round(process.memoryUsage().rss / 1024 / 1024)}MB`)
+
+    TEST = `- Finding all ${dimCubed} triples in the default graph ${dimSquared * 1} times\t(0 variables)\t`
+    console.time(TEST)
+    for (i = 0; i < dim; i++) {
+      for (j = 0; j < dim; j++) {
+        for (k = 0; k < dim; k++) {
+          assert.strict.equal(dataset.match(prefixed(i), prefixed(j), prefixed(k), '').size, 1)
+        }
+      }
+    }
+    console.timeEnd(TEST)
+
+    TEST = `- Finding all ${dimCubed} triples in the default graph ${dimSquared * 2} times\t(1 variable)\t`
+    console.time(TEST)
+    for (i = 0; i < dim; i++) {
+      for (j = 0; j < dim; j++) {
+        assert.strict.equal(dataset.match(prefixed(i), prefixed(j), null, '').size, dim)
+      }
+    }
+    for (i = 0; i < dim; i++) {
+      for (j = 0; j < dim; j++) {
+        assert.strict.equal(dataset.match(prefixed(i), null, prefixed(j), '').size, dim)
+      }
+    }
+    for (i = 0; i < dim; i++) {
+      for (j = 0; j < dim; j++) {
+        assert.strict.equal(dataset.match(null, prefixed(i), prefixed(j), '').size, dim)
+      }
+    }
+    console.timeEnd(TEST)
+
+    TEST = `- Finding all ${dimCubed} triples in the default graph ${dimSquared * 3} times\t(2 variables)\t`
+    console.time(TEST)
+    for (i = 0; i < dim; i++) {
+      assert.strict.equal(dataset.match(prefixed(i), null, null, '').size, dimSquared)
+    }
+    for (j = 0; j < dim; j++) {
+      assert.strict.equal(dataset.match(null, prefixed(j), null, '').size, dimSquared)
+    }
+    for (k = 0; k < dim; k++) {
+      assert.strict.equal(dataset.match(null, null, prefixed(k), '').size, dimSquared)
+    }
+    console.timeEnd(TEST)
+
+    console.log()
+
+    /* Test quads */
+    const quadDim = dim / 4
+    const quadDimCubed = quadDim ** 3
+    const dimQuads = quadDimCubed * quadDim
+
+    dataset = factory.dataset()
+    TEST = `- Adding ${dimQuads} quads`
+    console.time(TEST)
+    for (i = 0; i < quadDim; i++) {
+      for (j = 0; j < quadDim; j++) {
+        for (k = 0; k < quadDim; k++) {
+          for (l = 0; l < quadDim; l++) {
+            dataset.add(
+              rdf.quad(
+                prefixed(i),
+                prefixed(j),
+                prefixed(k),
+                prefixed(l)
+              )
+            )
+          }
+        }
+      }
+    }
+    console.timeEnd(TEST)
+
+    console.log(`* Memory usage for quads: ${Math.round(process.memoryUsage().rss / 1024 / 1024)}MB`)
+
+    TEST = `- Finding all ${dimQuads} quads ${quadDimCubed * 4} times\t(3 variables)\t`
+    console.time(TEST)
+    for (i = 0; i < quadDim; i++) {
+      assert.strict.equal(dataset.match(prefixed(i), null, null, null).size, quadDimCubed)
+    }
+    for (j = 0; j < quadDim; j++) {
+      assert.strict.equal(dataset.match(null, prefixed(j), null, null).size, quadDimCubed)
+    }
+    for (k = 0; k < quadDim; k++) {
+      assert.strict.equal(dataset.match(null, null, prefixed(k), null).size, quadDimCubed)
+    }
+    for (l = 0; l < quadDim; l++) {
+      assert.strict.equal(dataset.match(null, null, null, prefixed(l)).size, quadDimCubed)
+    }
+    console.timeEnd(TEST)
   }
 }
-console.timeEnd(TEST)
-
-console.log(`* Memory usage for quads: ${Math.round(process.memoryUsage().rss / 1024 / 1024)}MB`)
-
-TEST = `- Finding all ${dimQuads} quads ${dimCubed * 4} times (3 variables)`
-console.time(TEST)
-for (i = 0; i < dim; i++) {
-  assert.strict.equal(dataset.match(prefixed(i), null, null, null).size, dimCubed)
-}
-for (j = 0; j < dim; j++) {
-  assert.strict.equal(dataset.match(null, prefixed(j), null, null).size, dimCubed)
-}
-for (k = 0; k < dim; k++) {
-  assert.strict.equal(dataset.match(null, null, prefixed(k), null).size, dimCubed)
-}
-for (l = 0; l < dim; l++) {
-  assert.strict.equal(dataset.match(null, null, null, prefixed(l)).size, dimCubed)
-}
-console.timeEnd(TEST)
